efe414ead3733e385d6784716719d5a7af7ff380
James Ngugi <53422918+ngugi-james@users.noreply.github.com>
2024-12-12 11:12:45 -0500

    Update product catalogue

--- src/data.rs
+++ src/data.rs
@@ -5,73 +5,45 @@ pub fn fetch_products(_settings: &Settings) -> Vec<Product> {
     vec![
         Product {
             id: 1,
-            name: "Contoso Catnip's Friend".to_string(),
-            price: 9.99,
-            description: "Watch your feline friend embark on a fishing adventure with Contoso Catnip's Friend toy. Packed with irresistible catnip and dangling fish lure.".to_string(),
-            image: "/catnip.jpg".to_string()
+            name: "Bose QuietComfort Ultra".to_string(),
+            price: 349.99,
+            description: "Immerse yourself in sound with the Bose QuietComfort Ultra wireless headphones, offering noise-canceling technology and premium audio quality.".to_string(),
+            image: "/BoseQuietComfortUltra.jpg".to_string()
         },
         Product {
             id: 2,
-            name: "Salty Sailor's Squeaky Squid".to_string(),
-            price: 6.99,
-            description: "Let your dog set sail with the Salty Sailor's Squeaky Squid. This interactive toy provides hours of fun, featuring multiple squeakers and crinkle tentacles.".to_string(),
-            image: "/squid.jpg".to_string()
+            name: "Samsung Galaxy S24 Ultra".to_string(),
+            price: 1199.99,
+            description: "Experience the ultimate smartphone with the Samsung Galaxy S24 Ultra, featuring a stunning display, powerful performance, and exceptional camera capabilities.".to_string(),
+            image: "/SamsungGalaxyS24Ultra.jpg".to_string()
         },
         Product {
             id: 3,
-            name: "Mermaid's Mice Trio".to_string(),
-            price: 12.99,
-            description: "Entertain your kitty with the Mermaid's Mice Trio. These adorable plush mice are dressed as mermaids and filled with catnip to captivate their curiosity.".to_string(),
-            image: "/mermaid.jpg".to_string()
+            name: "LG 32ML600M Monitor".to_string(),
+            price: 249.99,
+            description: "Enhance your workspace or gaming setup with the LG 32ML600M monitor, offering a Full HD display and excellent color accuracy.".to_string(),
+            image: "/LG32ML600MMonitor.jpg".to_string()
         },
         Product {
             id: 4,
-            name: "Ocean Explorer's Puzzle Ball".to_string(),
-            price: 11.99,
-            description: "Challenge your pet's problem-solving skills with the Ocean Explorer's Puzzle Ball. This interactive toy features hidden compartments and treats, providing mental stimulation and entertainment.".to_string(),
-            image: "/ocean.jpg".to_string()
+            name: "Playstation 5 Slim Console".to_string(),
+            price: 499.99,
+            description: "Level up your gaming with the Playstation 5 Slim Console, delivering lightning-fast loading times and immersive gaming experiences.".to_string(),
+            image: "/PlayStation5SlimConsole.jpg".to_string()
         },
         Product {
             id: 5,
-            name: "Pirate Parrot Teaser Wand".to_string(),
-            price: 8.99,
-            description: "Engage your cat in a playful pursuit with the Pirate Parrot Teaser Wand. The colorful feathers and jingling bells mimic the mischievous charm of a pirate's parrot.".to_string(),
-            image: "/pirate.jpg".to_string()
+            name: "Apple Watch SE 44mm".to_string(),
+            price: 279.99,
+            description: "Stay connected and track your fitness goals with the Apple Watch SE 44mm, combining powerful features with a sleek design.".to_string(),
+            image: "/AppleWatchSE.jpg".to_string()
         },
         Product {
             id: 6,
-            name: "Seafarer's Tug Rope".to_string(),
-            price: 14.99,
-            description: "Tug-of-war meets nautical adventure with the Seafarer's Tug Rope. Made from marine-grade rope, it's perfect for interactive play and promoting dental health in dogs.".to_string(),
-            image: "/tug.jpg".to_string()
-        },
-        Product {
-            id: 7,
-            name: "Seashell Snuggle Bed".to_string(),
-            price: 19.99,
-            description: "Give your furry friend a cozy spot to curl up with the Seashell Snuggle Bed. Shaped like a seashell, this plush bed provides comfort and relaxation for cats and small dogs.".to_string(),
-            image: "/bed.jpg".to_string()
-        },
-        Product {
-            id: 8,
-            name: "Nautical Knot Ball".to_string(),
-            price: 7.99,
-            description: "Unleash your dog's inner sailor with the Nautical Knot Ball. Made from sturdy ropes, it's perfect for fetching, tugging, and satisfying their chewing needs.".to_string(),
-            image: "/knot.jpg".to_string()
-        },
-        Product {
-            id: 9,
-            name: "Contoso Claw's Crabby Cat Toy".to_string(),
-            price: 3.99,
-            description: "Watch your cat go crazy for Contoso Claw's Crabby Cat Toy. This crinkly and catnip-filled toy will awaken their hunting instincts and provide endless entertainment.".to_string(),
-            image: "/crabby.jpg".to_string()
-        },
-        Product {
-            id: 10,
-            name: "Ahoy Doggy Life Jacket".to_string(),
-            price: 5.99,
-            description: "Ensure your furry friend stays safe during water adventures with the Ahoy Doggy Life Jacket. Designed for dogs, this flotation device offers buoyancy and visibility in style.".to_string(),
-            image: "/lifejacket.jpg".to_string()
+            name: "JBL Boombox 2 Portable".to_string(),
+            price: 399.99,
+            description: "Take the party anywhere with the JBL Boombox 2 Portable Bluetooth Speaker, offering powerful sound and an impressive battery life.".to_string(),
+            image: "/JBLBoombox2Portable.jpg".to_string()
         }
     ]
 }
